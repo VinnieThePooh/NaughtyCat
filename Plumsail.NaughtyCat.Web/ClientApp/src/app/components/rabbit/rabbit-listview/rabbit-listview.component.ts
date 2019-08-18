d3fd7545b104beb339e4bd42d0e93346c05d7cb2
Ryan <lashkevichrain@gmail.com>
2019-08-18 16:08:29 +0700

    ExpressionChangedAfterItHasBeenCheckedError fixed

--- Plumsail.NaughtyCat.Web/ClientApp/src/app/components/rabbit/rabbit-listview/rabbit-listview.component.ts
+++ Plumsail.NaughtyCat.Web/ClientApp/src/app/components/rabbit/rabbit-listview/rabbit-listview.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  OnInit,
+  DoCheck,
+  AfterContentChecked,
+  ChangeDetectorRef
+} from "@angular/core";
 import { RabbitService } from "src/app/services/rabbit.service";
-import { MatTableDataSource } from "@angular/material/table";
-import { Observable } from "rxjs";
 import { Rabbit } from "src/app/models/rabbit";
-import { PageEvent } from "@angular/material";
+import { PageEvent, MatPaginator } from "@angular/material";
 
 @Component({
   selector: "ncat-rabbit-listview",
@@ -14,7 +21,8 @@ export class RabbitListviewComponent implements OnInit {
   rabbitsDataSource: Rabbit[];
   totalRecordsCount: number;
   pageSize: number;
-  isDataLoading: boolean;
+  isDataLoading: boolean = true;
+  pageIndex: number;
 
   columnsToDisplay: Array<string> = [
     // "id",
@@ -25,26 +33,50 @@ export class RabbitListviewComponent implements OnInit {
     "updateDate"
   ];
 
-  constructor(private rabbitService: RabbitService) {}
+  constructor(
+    private rabbitService: RabbitService,
+    private cdRef: ChangeDetectorRef
+  ) {}
+
+  private paginator: MatPaginator;
+
+  @ViewChild(MatPaginator, { static: false }) set content(
+    content: MatPaginator
+  ) {
+    if (content) {
+      this.paginator = content;
+      this.updatePaginator(this.paginator);
+    }
+  }
 
   get hasRabbits() {
     return this.rabbitsDataSource && this.rabbitsDataSource.length;
   }
 
-  ngOnInit() {
-    this.isDataLoading = true;
+  ngOnInit(): void {
+    console.log("ngOnInit event hook called");
     this.rabbitService.getRabbits().subscribe(
       r => {
+        this.isDataLoading = false;
+
         this.rabbitsDataSource = r.pageData;
         this.pageSize = r.pageSize;
         this.totalRecordsCount = r.totalRecordsCount;
-        this.isDataLoading = false;
+        this.pageIndex = r.pageNumber - 1;
       },
       e => {
         this.isDataLoading = false;
         throw e;
       }
     );
+  }
+
+  updatePaginator(p) {
+    setTimeout(() => {
+      p.pageSize = this.pageSize;
+      p.pageIndex = this.pageIndex;
+      p.length = this.totalRecordsCount;
+    });
   }
 
   addNewRabbit() {
